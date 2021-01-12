77828896ecc8aae226dd224050dcab5a58547d44
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-01-12 10:10:10 +0200

    Solved the sidebar problem

---
+++ src/blog/templatetags/post_tag.py
@@ -0,0 +1,19 @@
+from django import template
+from blog.models import post,comment
+register = template.Library()
+
+@register.inclusion_tag('blog/latest_posts.html')
+
+def latest_posts():
+    context ={
+        'l_posts':post.objects.all()[0:5]
+    }
+    return context
+
+@register.inclusion_tag('blog/latest_comments.html')
+def latest_comments():
+
+    context={
+    'l_comments': comment.objects.filter(active=True)[0:5]
+    }
+    return context
