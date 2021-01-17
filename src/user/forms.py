b3264e1e35e5cf4ca9e8d3320c91bb500c0bead4
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-01-17 13:26:31 +0200

    created profile update page

--- src/user/forms.py
+++ src/user/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from .models import Profile
 
 class UserCreationForm(forms.ModelForm):
     username = forms.CharField(label='اسم المستخدم',max_length=30, help_text='اسم المستخدم يجب الا يحتوي علي مسافات')
@@ -26,6 +26,11 @@ class UserCreationForm(forms.ModelForm):
             raise forms.ValidationError('يوجد مستخدم مسجل بهاذا الاسم')
         return cd['username']
 
+    def clean_email(self):
+        cd = self.cleaned_data
+        if User.objects.filter(email=cd['email']).exists():
+            raise forms.ValidationError('هذا الايميل مسجل من قبل')
+            return cd['email']
 
 class LoginForm(forms.ModelForm):
     username = forms.CharField(label='اسم المستخدم', help_text='اسم المستخدم يجب الا يحتوي علي اي مسافات')
@@ -34,3 +39,17 @@ class LoginForm(forms.ModelForm):
         
         model = User
         fields = ('username', 'password')
+
+
+class UserUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(label='الاسم الاول')
+    last_name = forms.CharField(label='الاسم الاخير')
+    email = forms.EmailField(label='البريد االكتروني')
+    class Meta:
+        model= User
+        fields= ('first_name', 'last_name', 'email')
+
+class ProfileUpdateForm(forms.ModelForm):
+        class Meta:
+            model= Profile
+            fields=(('image',))
--- src/user/urls.py
+++ src/user/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.Login_user, name='login'),
     path('logout/', views.Logout_user, name='logout'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('profile_update/', views.profile_update, name='profile_update')
 
 ] + static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
