979cb90269e739fb8c1df95e1e35f768d8f4eadc
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-01-17 21:06:07 +0200

    hostind project

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ctpma6z+y@gt@^kym_s=e=6fq4_1nm3=xhfa)hn+n^jmgx8ayg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
